8186085d63bf3c6b10967972e1f8b9a0f0426330
pedrotancredo <93741266+pedrotancredo@users.noreply.github.com>
2023-12-04 11:57:56 -0300

    Update documentation

--- pages/2_🎫_Pré_Anotação.py
+++ pages/2_🎫_Pré_Anotação.py
@@ -1,34 +1,36 @@
 import streamlit as st
 
-st.markdown('# Pré anotação:')
-st.markdown('''Para agilizar o processo de anotação, utilizaremos como premissa
-            algumas expressões regulares. Para isso, vamos criar o dicionário
-            indicado abaixo:''')
+st.markdown("# Pré anotação:")
+st.markdown(
+    """Antes do início do processo de anotação, foi utilizada uma abordagem de
+    pré-anotação através do uso de algumas expressões regulares. Destacadas abaixo:"""
+)
 
-TABLE = {'Descrição': {
-    'Localização:': 'torre ou vão seguido de número',
-    'Evento': 'inspeção seguido ou não de detalhada ou aérea',
-    'Componente (esfera)': 'esferas ou esfera seguido ou não de sinalização',
-    'Componente (espaçador)': 'espaçador ou espaçadores',
-    'Componente (cadeia)': 'cadeia opcional seguido por isolador(es)',
-    'Componente (faixa)': 'faixa seguido opcionalmente por de servidão',
-    'Componente (eucalipto)': 'eucalipto ou eucaliptos',
-    'Anomalia (quebrado)': 'quebrado ou quebrados',
-    'Anomalia (ninho)': 'ninho ou ninhos seguido por: pássaro, curicaca, gavião ou gaviões',
-    'Posição (fase)': 'fase seguido opcionalmente por lateral esquerda ou direita ou central ou do meio ou de baixo ou de cima ou ré ou a vante',
-},
-    'REGEX': {
-        'Localização:': r'(?:[Tt]orre|[Vv]ão)\s+\d+\b[.,]?',
-        'Evento': r'\b[Ii]nspeção[,.]?(?:\s[dD]etalhada|\s[aA]érea)?\b[.,]?',
-        'Componente (esfera)': r'\b[Ee]sferas?[,.]?(?:\sde[.,]?\s[sS]inalização)\b?[,.]?',
-        'Componente (espaçador)': r'\b[Ee]spaçador(?:es)?\b[.,]?',
-        'Componente (cadeia)': r'\b(?:[Cc]adeia de )?[Ii]solador(?:es)?\b[.,]?',
-        'Componente (faixa)': r'\b[Ff]aixa[.,]?(?: de servidão)?\b[.,]?',
-        'Componente (eucalipto)': r'\b[Ee]ucaliptos?\b[.,]?',
-        'Anomalia (quebrado)': r'\b[Qq]uebrad(?:o|a)s?\b[.,]?',
-        'Anomalia (ninho)': r'\b[Nn]inhos?(?: de (?:[Pp]assarinho|[Pp]ássaro|[Cc]uricaca|[Gg]avião|[Gg]aviões))?\b[.,?]?',
-        'Posição (fase)': r'(?:\b[Ff]ases?(?:[,.]? ?lateral)?(?: (?:esquerd[ao]|direit[ao]))?(?: central| do meio| de baixo| de cima| inferior| superior)?[.,]?(?: ré| a?vante)?\b[.,]?)|(?:\b(?:lateral)(?: (?:esquerd[ao]|direit[ao]))?(?: central| do meio| de baixo| de cima| inferior| superior)?[.,]?(?: ré| a?vante)?\b[.,]?)',
-}
+TABLE = {
+    "Descrição": {
+        "Localização:": "torre ou vão seguido de número",
+        "Evento": "inspeção seguido ou não de detalhada ou aérea",
+        "Componente (esfera)": "esferas ou esfera seguido ou não de sinalização",
+        "Componente (espaçador)": "espaçador ou espaçadores",
+        "Componente (cadeia)": "cadeia opcional seguido por isolador(es)",
+        "Componente (faixa)": "faixa seguido opcionalmente por de servidão",
+        "Componente (eucalipto)": "eucalipto ou eucaliptos",
+        "Anomalia (quebrado)": "quebrado ou quebrados",
+        "Anomalia (ninho)": "ninho ou ninhos seguido por: pássaro, curicaca, gavião ou gaviões",
+        "Posição (fase)": "fase seguido opcionalmente por lateral esquerda ou direita ou central ou do meio ou de baixo ou de cima ou ré ou a vante",
+    },
+    "REGEX": {
+        "Localização:": r"(?:[Tt]orre|[Vv]ão)\s+\d+\b[.,]?",
+        "Evento": r"\b[Ii]nspeção[,.]?(?:\s[dD]etalhada|\s[aA]érea)?\b[.,]?",
+        "Componente (esfera)": r"\b[Ee]sferas?[,.]?(?:\sde[.,]?\s[sS]inalização)\b?[,.]?",
+        "Componente (espaçador)": r"\b[Ee]spaçador(?:es)?\b[.,]?",
+        "Componente (cadeia)": r"\b(?:[Cc]adeia de )?[Ii]solador(?:es)?\b[.,]?",
+        "Componente (faixa)": r"\b[Ff]aixa[.,]?(?: de servidão)?\b[.,]?",
+        "Componente (eucalipto)": r"\b[Ee]ucaliptos?\b[.,]?",
+        "Anomalia (quebrado)": r"\b[Qq]uebrad(?:o|a)s?\b[.,]?",
+        "Anomalia (ninho)": r"\b[Nn]inhos?(?: de (?:[Pp]assarinho|[Pp]ássaro|[Cc]uricaca|[Gg]avião|[Gg]aviões))?\b[.,?]?",
+        "Posição (fase)": r"(?:\b[Ff]ases?(?:[,.]? ?lateral)?(?: (?:esquerd[ao]|direit[ao]))?(?: central| do meio| de baixo| de cima| inferior| superior)?[.,]?(?: ré| a?vante)?\b[.,]?)|(?:\b(?:lateral)(?: (?:esquerd[ao]|direit[ao]))?(?: central| do meio| de baixo| de cima| inferior| superior)?[.,]?(?: ré| a?vante)?\b[.,]?)",
+    },
 }
 
 st.dataframe(TABLE)
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,47 +3,176 @@ from PIL import Image
 import streamlit as st
 
 
-image = Image.open('images/banner.png')
+image = Image.open("images/banner.png")
 st.image(image, use_column_width=True)
 
 
-st.header('Modelo para reconhecimento de entidades nomeadas em transcrições de audios de inspeção aérea de linhas de transmissão')
-st.markdown('<div align="right">Pedro Henrique Tancredo Campos</div>', unsafe_allow_html=True)
+st.header(
+    "Modelo para reconhecimento de entidades nomeadas (NER) em transcrições de audios de inspeção aérea de linhas de transmissão"
+)
+st.markdown(
+    '<div align="right">Pedro Henrique Tancredo Campos</div>', unsafe_allow_html=True
+)
 
-tabs = st.tabs(['Introdução', 'Modelo', 'Classes'])
+tabs = st.tabs(["Introdução", "Dataset", "Modelo"])
 
 with tabs[0]:
-    st.subheader('Inspeção Aérea de Linhas de Transmissão')
-    st.markdown('''<div align="justify"><p>A infraestrutura elétrica é a espinha dorsal de qualquer sociedade moderna, impulsionando o desenvolvimento e sustentando o modo de vida contemporâneo. No cerne dessa infraestrutura, as linhas de transmissão de energia desempenham um papel crucial, transportando eletricidade de usinas geradoras para comunidades e empresas. Para garantir a confiabilidade e a eficiência dessas linhas, a inspeção aérea emerge como uma prática essencial.</p></div>''', unsafe_allow_html=True)
+    st.subheader("Inspeção Aérea de Linhas de Transmissão")
+    st.markdown(
+        """<div align="justify"><p>A infraestrutura elétrica é a espinha dorsal
+        de qualquer sociedade moderna, impulsionando o desenvolvimento e
+        sustentando o modo de vida contemporâneo. No cerne dessa infraestrutura,
+        as linhas de transmissão de energia desempenham um papel crucial,
+        transportando eletricidade de usinas geradoras para comunidades e empresas.
+        Para garantir a confiabilidade e a eficiência dessas linhas, a inspeção
+        aérea emerge como uma prática essencial.</p></div>""",
+        unsafe_allow_html=True,
+    )
 
-    # with st.expander(label="O que é inspeção aérea de linhas de transmissão?"):
-    st.markdown('''<div align="justify">
-    <p>A inspeção aérea de linhas de transmissão envolve o uso de aeronaves especializadas equipadas com tecnologias capazes de avaliar a condição física e operacional das estruturas. Esta abordagem oferece vantagens significativas em comparação com métodos terrestres, permitindo uma análise abrangente e eficiente em grandes extensões de território.</p>
-    <p>Um dos benefícios fundamentais da inspeção aérea é a capacidade de identificar de forma rápida e precisa qualquer anomalia ou desgaste nas estruturas das linhas de transmissão. Os helicópteros equipados com câmeras de alta resolução e sensores específicos podem detectar sinais de corrosão, danos mecânicos ou outros problemas que podem comprometer a integridade da linha.</p>
-    <p>Além disso, a inspeção aérea facilita a identificação precoce de vegetação excessiva nas proximidades das linhas. A vegetação não gerenciada pode representar riscos substanciais, incluindo interrupções no fornecimento de energia. Com a capacidade de sobrevoar grandes áreas rapidamente, a inspeção aérea permite a pronta detecção e mitigação desses riscos ambientais.</p>
-    <p>A rapidez na identificação e resolução de problemas é crucial para garantir a confiabilidade operacional das linhas de transmissão. A inspeção aérea, ao fornecer uma visão panorâmica e detalhada, acelera o processo de manutenção, reduzindo significativamente o tempo de inatividade e os custos associados.</p>
-    </div>
-    ''', unsafe_allow_html=True)
+    st.markdown(
+        """<div align="justify">
+        <p>A inspeção aérea de linhas de transmissão envolve o uso de aeronaves
+        especializadas equipadas com tecnologias capazes de avaliar a condição
+        física e operacional das estruturas. Esta abordagem oferece vantagens
+        significativas em comparação com métodos terrestres, permitindo uma
+        análise abrangente e eficiente em grandes extensões de território.</p>
+        <p>Um dos benefícios fundamentais da inspeção aérea é a capacidade de
+        identificar de forma rápida e precisa qualquer anomalia ou desgaste nas
+        estruturas das linhas de transmissão. Os helicópteros equipados com
+        câmeras de alta resolução e sensores específicos podem detectar sinais
+        de corrosão, danos mecânicos ou outros problemas que podem comprometer
+        a integridade da linha.</p>
+        <p>Além disso, a inspeção aérea facilita a identificação precoce de
+        vegetação excessiva nas proximidades das linhas. A vegetação nãogerenciada
+        pode representar riscos substanciais, incluindo interrupções no fornecimento
+        de energia. Com a capacidade de sobrevoar grandes áreas rapidamente, a
+        inspeção aérea permite a pronta detecção e mitigação desses riscos
+        ambientais.</p>
+        <p>A rapidez na identificação e resolução de problemas é crucial para
+        garantir a confiabilidade operacional das linhas de transmissão. A inspeção
+        aérea, ao fornecer uma visão panorâmica e detalhada, acelera o processo
+        de manutenção, reduzindo significativamente o tempo de inatividade e os
+        custos associados.</p></div>""",
+        unsafe_allow_html=True,
+    )
 
-    st.subheader('Inspeção instrumentalizada')
+    st.subheader("Instrumentalização")
 
-    st.markdown('''<div align="justify"><p>Como uma evolução dos métodos tradicionais de inspeção aérea, a instrumentalização, utilizando câmeras e áudio, representou um avanço significativo na manutenção e monitoramento dessa infraestrutura. Ao incorporar tecnologias audiovisuais, esta abordagem não apenas proporcionou uma análise visual detalhada, mas também ofereceu informações auditivas valiosas que podem ser exploradas de maneiras inovadoras.</p></div>''', unsafe_allow_html=True)
-    st.markdown('''<div align="justify"><p>O uso de câmeras de alta resolução montadas em drones ou helicópteros permite a captura de registros em vídeo durante a inspeção. Essas imagens não apenas fornecem uma visão clara e detalhada do estado físico das linhas, mas também possibilitam uma revisão mais minuciosa posteriormente. A análise de vídeos pode revelar nuances que podem escapar à observação instantânea, facilitando a identificação de desgastes, danos ou potenciais pontos de falha.</p>
-    <p>A verdadeira inovação surge quando exploramos o potencial da transcrição automática do áudio. Ao converter os registros de áudio em texto, obtemos um recurso adicional para a análise e documentação. Essa transcrição não apenas facilita a catalogação e indexação eficientes das inspeções, mas também permite a aplicação de técnicas de processamento de linguagem natural para identificar padrões e tendências ao longo do tempo.</p>
-    <p>Ao combinar dados visuais e auditivos, as empresas de energia podem desenvolver estratégias mais abrangentes de manutenção preditiva. A análise conjunta de imagens e transcrições permite uma compreensão mais holística da saúde das linhas de transmissão, capacitando as equipes de manutenção a antecipar problemas potenciais e agir proativamente.
-    Em última análise, a inspeção instrumentalizada, incorporando elementos visuais e auditivos, não apenas eleva a eficiência da manutenção de linhas de transmissão, mas também abre portas para a implementação de soluções inovadoras e inteligentes, fundamentadas na análise integrada de dados multimodais.</p></div>''', unsafe_allow_html=True)
+    st.markdown(
+        """<div align="justify"><p>Como uma evolução dos métodos tradicionais de
+        inspeção aérea, a instrumentalização, utilizando câmeras e áudio,
+        representou um avanço significativo na manutenção e monitoramento dessa
+        infraestrutura. Ao incorporar tecnologias audiovisuais, esta abordagem
+        não apenas proporcionou uma análise visual detalhada, mas também ofereceu
+        informações auditivas valiosas que podem ser exploradas de maneiras
+        inovadoras.</p></div>""",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        """<div align="justify"><p>O uso de câmeras de alta resolução montadas
+        em drones ou helicópteros permite a captura de registros em vídeo
+        durante a inspeção. Essas imagens não apenas fornecem uma visão clara
+        e detalhada do estado físico das linhas, mas também possibilitam uma
+        revisão mais minuciosa posteriormente. A análise de vídeos pode revelar
+        nuances que podem escapar à observação instantânea, facilitando a
+        identificação de desgastes, danos ou potenciais pontos de falha.</p>
+        <p>A verdadeira inovação surge quando exploramos o potencial da transcrição
+        automática do áudio. Ao converter os registros de áudio em texto, obtemos
+        um recurso adicional para a análise e documentação. Essa transcrição não
+        apenas facilita a catalogação e indexação eficientes das inspeções, mas
+        também permite a aplicação de técnicas de processamento de linguagem
+        natural para identificar padrões e tendências ao longo do tempo.</p>
+        <p>Ao combinar dados visuais e auditivos, as empresas de energia podem
+        desenvolver estratégias mais abrangentes de manutenção preditiva. A
+        análise conjunta de imagens e transcrições permite uma compreensão mais
+        holística da saúde das linhas de transmissão, capacitando as equipes de
+        manutenção a antecipar problemas potenciais e agir proativamente. Em
+        última análise, a inspeção instrumentalizada, incorporando elementos
+        visuais e auditivos, não apenas eleva a eficiência da manutenção de
+        linhas de transmissão, mas também abre portas para a implementação de
+        soluções inovadoras e inteligentes, fundamentadas na análise integrada
+        de dados multimodais.</p></div>""",
+        unsafe_allow_html=True,
+    )
+with tabs[1]:
+    st.subheader("Dataset")
+    st.markdown(
+        """<div align="justify"><p>O conjunto de dados utilizado  para o NER
+        constitui-se das transcrições dos audios das inspeções aéreas de linhas
+        de transmissão onde os inspetores relatam as anomalias encontradas. Com
+        base na observação de diversos exemplos destas transcrições, foram definidas
+        as seguintes classes:</p></div>""",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        """<div align="justify"><p>
+        <li><b>Componente</b>: Partes que compõem a estrutura da linha de transmissão.
+        <ul><b>Exemplos:</b> Isolador, Espaçador, Esfera, Cone, Amortecedor,
+        Faixa de Servidão</ul></li>
+        <li><b>Anomalia</b>: Possíveis defeitos ou anomalias que podem ser
+        identificados durante a inspeção aérea.
+        <ul><b>Exemplos:</b> Quebrado, Caído, Ninho, Frouxo</ul></li>
+        <li><b>Posição</b>: Posição relativa das anomalias em relação à estrutura da
+        linha de transmissão.
+        <ul><b>Exemplos:</b> Ré, Vante, Fase Central, Fase Superior, Mísula</ul></li>
+        <li><b>Linha de Transmissão</b>: Nome específico da linha de transmissão
+        sob inspeção.
+        <ul><b>Exemplos:</b> Furnas - Pimenta, Campos - Rio Novo do Sul 1</ul></li>
+        <li><b>Evento</b>: Eventos específicos relacionados à inspeção.
+        <ul><b>Exemplos:</b>Inspeção Detalhada</ul></li>
+        <li><b>Localização</b>: Localização exata das entidades identificadas.
+        <ul><b>Exemplos:</b> Torre 247, Torre 123, Vão 210, Vão 109</ul></li>
+        </p></div>""",
+        unsafe_allow_html=True,
+    )
+    st.markdown("#### Exemplos de Transcrições:")
 
-# with st.expander("A inspeção instrumentalizada em Furnas"):
-
+    st.markdown(
+        '''<div align="justify">
+        <p><b>Transcrição 1:</b> Um isolador quebrado, linha Furnas - Pimenta um,
+        cadeia simples.</p>
+        <p><b>Transcrição 2:</b> Esfera desbotada no Vão 210 da linha Adrianópolis -
+        Jacarepaguá 2.</p>
+        <p><b>Transcrição 3:</b> Espaçador aberto na fase lateral esquerda do vão
+        30 na LT Marimbondo - Porto Colômbia."''',
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        """<div align="justify"><p>É importante destacar que o conjunto de dados
+        foi gerado a partir das transcrições automáticas dos áudios de inspeções
+        aéreas sem a imposição de orientações específicas para os inspetores.
+        Dessa forma, o conjunto de dados incorpora uma variedade de desafios
+        decorrentes da abordagem diversificada de cada inspetor ao relatar as
+        anomalias observadas. Este aspecto introduz diferentes nuances na maneira
+        como as inspeções são documentadas e apresenta desafios valiosos para o
+        desenvolvimento do modelo de Reconhecimento de Entidades Nomeadas (NER).</p>
+        <p><b>Diversidade nas Descrições:</b> A diversidade nas descrições das
+        anomalias é uma característica fundamental do conjunto de dados. Cada
+        inspetor tem sua própria abordagem ao relatar, resultando em uma gama
+        abrangente de expressões e terminologias.</p>
+        <p><b>Desafios de Consistência:</b> A ausência de orientações prévias
+        para os inspetores pode gerar inconsistências nas transcrições. Essas
+        inconsistências são valiosas para entender como diferentes abordagens
+        impactam a qualidade e uniformidade das informações registradas.</p>
+        <p>Espera-se que, ao analisar o conjunto de dados sob essa perspectiva
+        diversificada, seja possível estabelecer diretrizes específicas e melhores
+        práticas para a padronização e aprimoramento do processo de transcrição.
+        A compreensão dessas nuances não apenas contribuirá para a melhoria contínua
+        do conjunto de dados, mas também fortalecerá a robustez do modelo de NER
+        em lidar com a variabilidade natural das inspeções aéreas.</p></div>""",
+        unsafe_allow_html=True,
+    )
 with tabs[2]:
-    st.subheader('Classes')
-    st.markdown('''
-    Pensei em dividir da seguinte forma, vou colocar uns exemplos também:
-
-    - componente: isolador, espaçador, esfera, cone, amortecedor, faixa de servidão
-    - anomalia: quebrado, caído, ninho, frouxo
-    - posição: ré, vante, fase central, fase superior, mísula
-    - linha de transmissão: Furnas - Pimenta, Campos - Rio Novo do Sul 1
-    - subestação: Campos, Adrianópolis, Pimenta, Angra, Marimbondo
-    - evento: Inspeção Detalhada
-    - localização: Torre 247, Torre 123, Vão 210, Vão 109''')
+    st.subheader("Modelo")
+    st.markdown(
+        """<div align="justify"><p>O Reconhecimento de Entidade Nomeada
+         (Named Entity Recognition - NER) refere-se à identificação de entidades
+         com categorias específicas (geralmente substantivos) do texto, como
+         nomes de pessoas, nomes de lugares, nomes de organizações, etc. O
+         reconhecimento de entidades nomeadas é uma tarefa básica de recuperação
+         de informações, classificação de consulta, pergunta e resposta, etc.
+         Seu efeito afeta diretamente o processamento subsequente, por isso é um
+         problema recorrente de pesquisa em processamento de linguagem natural.
+         </p></div>""",
+        unsafe_allow_html=True,
+    )
